7aa329b349235e506316db77a2f9314c85ac8af8
Prabhat Rathva <144404271+Prabhat99125@users.noreply.github.com>
2024-07-12 18:27:16 -0700

    Update arduinowithHCsr04andLED.ino

--- arduinowithHCsr04andLED.ino
+++ arduinowithHCsr04andLED.ino
@@ -1,94 +1,34 @@
-int Time ;
-int distance;
-
-void setup()
-{
- pinMode(2,OUTPUT);
- pinMode(3,INPUT);
- pinMode(4,OUTPUT);
- pinMode(5,OUTPUT);
- pinMode(6,OUTPUT);
- pinMode(7,OUTPUT);
- pinMode(8,OUTPUT);
- Serial.begin(9600);
+const int TRIG_PIN = 6; 
+const int ECHO_PIN = 7; 
+const int Buzzer_PIN  = 11; 
+const int DISTANCE_THRESHOLD = 18; 
 
 
+float duration_us, distance_cm;
+
+void setup() {
+  Serial.begin (9600);       
+  pinMode(TRIG_PIN, OUTPUT); 
+  pinMode(ECHO_PIN, INPUT);   
+  pinMode(Buzzer_PIN, OUTPUT);  
 }
 
-void loop() 
-{
- digitalWrite(2,HIGH);
- delayMicroseconds(10);
- digitalWrite(2,LOW);
- delayMicroseconds(5);
- digitalWrite(2,HIGH);
- delayMicroseconds(10);
+void loop() {
+  
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
 
-Time = pulseIn(3,HIGH);
+  
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+  
+  distance_cm = 0.017 * duration_us;
 
-    distance= 0.0343*Time/2;
-    Serial.print("distane from sensor to object is =");
-    Serial.println(distance);
-    delay(5);
+  if(distance_cm < DISTANCE_THRESHOLD)
+    digitalWrite(Buzzer_PIN, HIGH); 
+  else
+    digitalWrite(Buzzer_PIN, LOW);  
 
-    
-   if(distance>1 && distance <=10 )
-   {
-   
-    digitalWrite(4,HIGH);
-    digitalWrite(5,HIGH);
-    digitalWrite(6,HIGH);
-    digitalWrite(7,HIGH);           
-    digitalWrite(8,HIGH);
-    }
-
-   if(distance>10 && distance <=15)
-   {
-    digitalWrite(4,HIGH);
-    digitalWrite(5,HIGH);
-    digitalWrite(6,HIGH);
-    digitalWrite(7,HIGH);            
-    digitalWrite(8,LOW);   
-   }
-
-
-   if(distance>15 && distance <=20)
-   {
-
-    digitalWrite(4,HIGH);
-    digitalWrite(5,HIGH);
-    digitalWrite(6,HIGH);
-    digitalWrite(7,LOW);            
-    digitalWrite(8,LOW);   
  
-   }
-
-   if(distance>20 && distance <=25)
-   {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-    digitalWrite(4,HIGH);
-    digitalWrite(5,HIGH);
-    digitalWrite(6,LOW);
-    digitalWrite(7,LOW);
-    digitalWrite(8,LOW);    
-    
-   
-   }
- if(distance>25 && distance <=30)
-   {
-    digitalWrite(4,HIGH);
-    digitalWrite(5,LOW);
-    digitalWrite(6,LOW);
-    digitalWrite(7,LOW);
-    digitalWrite(8,LOW);   
-   }
- 
-    if(distance>30)
-   {
-    digitalWrite(4,LOW);
-    digitalWrite(5,LOW);
-    digitalWrite(6,LOW);
-    digitalWrite(7,LOW);           
-    digitalWrite(8,LOW);
-    
-   }
-}
+  Serial.print("distance: ");
+  Serial.print(distance_cm);
